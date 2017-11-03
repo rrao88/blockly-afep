a42e01e7076634d0aa27c76521fe083f646e8d22
rrao88 <rahulrao2001@hotmail.com>
2017-11-03 13:24:35 +0100

    corrected type dependencies

--- blocks/contract/contract.js
+++ blocks/contract/contract.js
@@ -1,27 +1,19 @@
-'use strict';
-
-goog.provide('Blockly.Solidity.contract');
-
 Blockly.Blocks['contract_main'] = {
-	init: function () {
+	init: function() {
 		this.appendDummyInput()
 			.appendField("KAUFVERTRAG")
 			.appendField(new Blockly.FieldTextInput("Name des Kaufvertrages"), "NAME");
 		this.appendDummyInput();
 		this.appendStatementInput("VERKAEUFER")
-			.setCheck("contract_verkaeufer")
 			.setAlign(Blockly.ALIGN_CENTRE)
 			.appendField("Verkäufer");
 		this.appendStatementInput("KAUFGEGENSTAND")
-			.setCheck("contract_gegenstand")
 			.setAlign(Blockly.ALIGN_CENTRE)
 			.appendField("Kaufgegenstand");
 		this.appendStatementInput("KAEUFER")
-			.setCheck("contract_kaeufer")
 			.setAlign(Blockly.ALIGN_CENTRE)
 			.appendField("Käufer");
 		this.appendStatementInput("LIEFERANT")
-			.setCheck("contract_lieferant")
 			.setAlign(Blockly.ALIGN_CENTRE)
 			.appendField("Lieferant");
 		this.setColour(159);
