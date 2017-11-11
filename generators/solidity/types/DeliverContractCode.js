d7c9b84c00abf722fb472d2a761e4c59dcc00da1
Philipp Dublanc <philipp.dublanc@gmx.net>
2017-11-11 23:27:56 +0100

    DeliverContract Kommentare ergänzt

--- generators/solidity/types/DeliverContractCode.js
+++ generators/solidity/types/DeliverContractCode.js
@@ -2,14 +2,20 @@ function getDeliveryContractCode(price, isAbortable){
 
 	var code = "contract DeliverContract is BaseContract {\n" +
 		"\n" +
+		
+		// Enum State und Variable state definieren
 		"\tenum State { Created, Locked, InDelivery, Inactive }\n" +
 		"\tState public state;\n" +
 		"\n" +
+		
+		// Modifier zum prüfen in Welchem State sich der Vertrag befindet
 		"\tmodifier inState(State _state) {\n" +
 		"\t\trequire(state == _state);\n" +
 		"\t\t_;\n" +
 		"\t}\n" +
 		"\n" +
+		
+		//Konstruktor für den DeliverContract
 		"\tfunction DeliverContract() payable public {\n" +
 		"\t\tseller = msg.sender;\n" +
 		"\t\tvalue = " + price + ";\n" +
@@ -17,21 +23,29 @@ function getDeliveryContractCode(price, isAbortable){
 		"\t\trequire((2 * value) == msg.value);\n" +
 		"\t}\n" +
 		"\n" +
+		
+		//Modifier zum prüfen ob der Sender der Transakton der Käufer ist
 		"\tmodifier onlyCarrier() {\n" +
 		"\t\trequire(msg.sender == buyer);\n" +
 		"\t\t_;\n" +
 		"\t}\n" +
 		"\n" +
+		
+		//Funktion zum kaufen
 		"\tfunction confirmPurchase() inState(State.Created) payable public {\n" +
 		"\t\tsuper.purchaseOrderReceived();\n" +
 		"\t\tstate = State.Locked;\n" +
 		"\t}\n" +
 		"\n" +
+		
+		//Funktion um den Erhalt der Ware zu bestätigen
 		"\tfunction confirmReceived() inState(State.InDelivery) public {\n" +
 		"\t\tstate = State.Inactive;\n" +
 		"\t\tsuper.orderReceivedConfirmed();\n" +
 		"\t}\n" +
 		"\n" +
+		
+		//Funktion um die Paketaufgabe zu bestätigen
 		"\tfunction confirmInTransit() onlyBuyer inState(State.Locked) public {\n" +
 		"\t\tstate = State.InDelivery;\n" +
 		"\t}\n" +
